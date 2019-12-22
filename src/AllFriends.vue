f280deb3cb6528733747a538cbd10b785561b711
devjosegomez <devjosegomez@gmail.com>
2019-12-22 15:50:06 -0600

    Added AllFriends.vue

---
+++ src/AllFriends.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="all-friends">
+        <h2>All friends</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AllFriends",
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
