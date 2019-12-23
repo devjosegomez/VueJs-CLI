34dc81307231efb38e1d058fe3f31a7dac921a9e
devjosegomez <devjosegomez@gmail.com>
2019-12-22 18:21:20 -0600

    Added Blogs.vue

--- src/Blogs.vue
+++ src/Blogs.vue
@@ -2,31 +2,35 @@
     <div class="blogs">
         <h2>{{ blogTitle }}</h2>
         <button @click="changeTitle">Change blog's title </button>
+        <div v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p><q>{{ post.body }}</q></p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
     name: "Blogs",
     data(){
         return {
-            blogTitle: "Blogs"
+            blogTitle: "Blogs",
+            posts: []
         }
     },
     methods:{
         changeTitle(){
             this.blogTitle = "beforeUpdate Blogs"
         }
-    }
-    ,
-    beforeCreate(){
-        alert("Before created")
     },
     created(){
-        alert("created")
-    },
-    beforeUpdate(){
-         alert("beforeUpdate")
+        axios.get("https://jsonplaceholder.typicode.com/posts/")
+        .then(response => {
+            //console.log(response)
+            this.posts = response.data
+            console.log(this.posts)
+        })
     }
 }
 </script>
